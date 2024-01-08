528afb404a78f172c81f584a5f39ab737ce86679
Jonathan Koranteng <kor22002@byui.edu>
2024-01-08 22:06:50 +0000

    Home page changes

---
+++ scripts/getDates.js
@@ -0,0 +1,11 @@
+//select the year element and last modified element
+let year = document.querySelector('#year');
+let lastModified = document.querySelector('#lastModified');
+
+//get current date and time
+const date = new Date();
+let currentYear = date.getFullYear(); 
+
+
+//display current date in year element
+year.innerHTML = currentYear;
