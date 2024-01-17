1d6b449d6d6345d6f2241f327ca1cbb43c0ea9cf
Jonathan Koranteng <kor22002@byui.edu>
2024-01-17 21:04:57 +0000

    Chamber home page

---
+++ scripts/responsivemenu.js
@@ -0,0 +1,7 @@
+//responsive menu
+/*const hamburgerElement = document.querySelector("#mybutton");
+const navElement = document.querySelectorAll(".menuLinks");
+
+hamburgerElement.addEventListener("click", () => {
+   navElement.classList.toggle(".open");
+});*/
