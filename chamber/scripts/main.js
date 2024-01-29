b3fecfd4a7a213519fa457b843a17dc97c814170
Jonathan Koranteng <kor22002@byui.edu>
2024-01-29 23:48:50 +0000

    Chamber home page

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -25,3 +25,20 @@ hamburgerElement.addEventListener('click', ()=> {
     navElement.classList.toggle('open');
     hamburgerElement.classList.toggle('open');
 });
+
+//local storage
+const displayDaysVisited = document.querySelector('#daysVisited');
+
+//get number of visits
+let visitsCount = Number(window.localStorage.getItem('visitNum')) || 0;
+
+if (visitsCount !== 0) {
+    displayDaysVisited.textContent = `You visited ${visitsCount} day/days ago.`;
+}else {
+    displayDaysVisited.textContent = `This is your first visit. Feel free to explore.`;
+}
+
+visitsCount++;
+
+//key='visitNum
+localStorage.setItem('visitNum', visitsCount);
